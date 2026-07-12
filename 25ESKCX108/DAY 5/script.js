28d1c9ad209869ecfc43ccfe97c45f09a4673cc8
swastikdhamija10-ux <swastikdhamija10@gmail.com>
2026-07-12 16:07:51 +0530

    Update script.js

--- 25ESKCX108/DAY 5/script.js
+++ 25ESKCX108/DAY 5/script.js
@@ -1,1 +1,9 @@
+let count = 0;
 
+let button = document.getElementById("clickBtn");
+let counter = document.getElementById("count");
+
+button.addEventListener("click", function () {
+    count++;
+    counter.innerHTML = count;
+});
