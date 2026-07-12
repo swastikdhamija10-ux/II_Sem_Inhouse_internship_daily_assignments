2a5c2bd4afe2a67d9359a033fc50d66b0bd6a4b1
swastikdhamija10-ux <swastikdhamija10@gmail.com>
2026-07-12 16:21:16 +0530

    Update footer.php

--- 25ESKCX108/DAY 10/footer.php
+++ 25ESKCX108/DAY 10/footer.php
@@ -1,1 +1,6 @@
-
+<footer class="bg-dark text-white text-center py-3 mt-5">
+    <p class="mb-0">&copy; <?php echo date("Y"); ?> MY Website. All Rights Reserved.</p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
