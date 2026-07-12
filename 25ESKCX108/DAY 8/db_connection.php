69be6bf55060dbc5c2573bc24aa9ba4368f25fb8
swastikdhamija10-ux <swastikdhamija10@gmail.com>
2026-07-12 16:09:43 +0530

    Update db_connection.php

--- 25ESKCX108/DAY 8/db_connection.php
+++ 25ESKCX108/DAY 8/db_connection.php
@@ -1,1 +1,14 @@
+<?php
+// Basic form handling and validation example
+$servername = "localhost";
+$username = "root"; // Replace with your database username
+$password = ""; // Replace with your database password
+$dbname = "skit";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+//check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully<br>";
 
+?>
