6ba1c7371cd85ec7748f9bb96b73fe7ace1acea0
swastikdhamija10-ux <swastikdhamija10@gmail.com>
2026-07-12 16:25:46 +0530

    Update dashboard.php

--- 25ESKCX108/DAY 11/dashboard.php
+++ 25ESKCX108/DAY 11/dashboard.php
@@ -1,1 +1,12 @@
+<?php
+include("dashboardheader.php");
+include("dashboardVerticalContent.php");
+?>
 
+<br>
+<h2 style="font-style: italic"> <?php echo "Welcome, " .$_SESSION['user_name']. "!";?> </h2>
+
+<?php
+include("dashboardfooter.php");
+include("footer.php");
+?>
